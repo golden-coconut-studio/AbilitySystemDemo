a4769e83facd1ba78578aafc6ea2ac5504e2c1ec
ingridwarrior2901 <ingridguerrero@protonmail.com>
2020-02-01 17:09:40 -0500

    Added demo project

---
+++ Source/AbilitySystemExample/AbilitySystemExample.Build.cs
@@ -0,0 +1,26 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class AbilitySystemExample : ModuleRules
+{
+	public AbilitySystemExample(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GCSAbilitySystem"});
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        
+        //PrivateIncludePaths.AddRange(new string[] { "GCSAbilitySystem/Private" });
+        //PublicIncludePaths.AddRange(new string[] { "GCSAbilitySystem/Public" });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/AbilitySystemExample/AbilitySystemExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AbilitySystemExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABILITYSYSTEMEXAMPLE_API AAbilitySystemExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
